de8bc01dc4e6b45db865d4aba7e8017aa353fb6d
Oksana-Pak <okspak7@gmail.com>
2022-07-26 11:13:07 +0300

    move btn in mobile menu in container

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,7 @@
-// (() => {
-//   const menuBtnRef = document.querySelector('[data-menu-button]');
-//   const mobileMenuRef = document.querySelector('[data-menu]');
-
-//   menuBtnRef.addEventListener('click', () => {
-//     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-//     menuBtnRef.classList.toggle('is-open');
-//     menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-//     mobileMenuRef.classList.toggle('is-open');
-//   });
-// })();
-
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('.mobile-menu__open'),
-    closeMenuBtn: document.querySelector('.mobile-menu__cross'),
+    closeMenuBtn: document.querySelector('.mobile-menu__close'),
     menu: document.querySelector('.mobile-menu'),
     // body: document.querySelector('[data-body]'),
   };
